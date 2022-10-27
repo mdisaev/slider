162c3231bf95d20932925b7065562bebb52882cd
mdisaev <mdisaev@gmail.com>
2022-10-27 16:54:43 +0300

    new

---
+++ app.js
@@ -0,0 +1,15 @@
+console.log("Hello world!");
+// for(var i = 0; i < 3; i++){
+//     setTimeout(function(){console.log(i)})
+// } 
+
+
+// for(let i = 0; i < 3; i++){
+//     setTimeout(function(){console.log(i)})
+// } => 0 1 2
+
+for(const i = 0; i < 3; i++){
+    console.log(i);
+}
+
+console.log(i);
